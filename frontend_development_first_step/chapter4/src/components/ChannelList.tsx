7a4971aaa7d3e8b5ad93e5c95873eace591ca1f7
Sihyung Lee <touchthesky80s@gmail.com>
2020-03-16 21:49:26 +0900

    React - channel list

---
+++ frontend_development_first_step/chapter4/src/components/ChannelList.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import {Menu, Icon} from 'semantic-ui-react';
+import {Link, NavLink} from 'react-router-dom';
+  
+const channels = ['general', 'random'];
+
+export const ChannelList = () => {    
+  return (
+    <Menu inverted vertical fixed={'left'}>
+      <Menu.Item as={Link} to={'/'}>
+        Home
+        <Icon name='home'/>
+      </Menu.Item>      
+      <Menu.Item>
+        Channes 
+        <Icon name='list'/>
+        <Menu.Menu>
+          {
+            channels.map(channel => 
+              <Menu.Item
+                key={channel}
+                name={channel}
+                as={NavLink}
+                to={{ pathname: `/channels/${channel}`}}>
+                {channel}
+              </Menu.Item>  
+            )
+          }
+        </Menu.Menu>
+      </Menu.Item>
+    </Menu>
+  );
+}
